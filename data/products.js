b243f864051a274e2f25b2ae5cb1f483e048fcae
BenFrancka <benfrancka@gmail.com>
2021-05-10 13:37:29 -0700

    writes object literals for products, creates an array of products

---
+++ data/products.js
@@ -0,0 +1,60 @@
+
+const enchiladas = {
+    id: 'enchiladas',
+    name: 'chicken enchiladas',
+    image : 'enchiladas.png',
+    description: 'Blue corn tortillas with pulled chicken, chile verde sauce, cotija cheese, and fresh cilantro',
+    category: 'Mexican',
+    price: 15.00,
+    cost: 4.50
+};
+
+const spaghetti= {
+    id: 'spaghetti',
+    name: 'Spaghetti with Pork Meatballs',
+    image : 'spaghetti-and-meatballs.png',
+    description: 'Handmade spaghetti noodles with classic red sauce, pork meatball kit, fresh basil, and parmigiano reggiano',
+    category: 'Italian',
+    price: 18.00,
+    cost: 5.50
+};
+
+const burger = {
+    id: 'burger',
+    name: 'Bison Cheeseburgers',
+    image : 'cheeseburger.png',
+    description: 'Grass fed bison patties with brioche buns, smoked gouda cheese, and quick pickle kit',
+    category: 'American',
+    price: 20.00,
+    cost: 6.50
+};
+
+const ramen = {
+    id: 'ramen',
+    name: 'Spicy Shoyu Ramen',
+    image : 'ramen.png',
+    description: 'Gourmet Instant Ramen kit with pork belly, chili oil, and eggs',
+    category: 'Japanese',
+    price: 19.00,
+    cost: 6.50
+};
+
+const sushi = {
+    id: 'sushi',
+    name: 'Spicy Tuna Rolls',
+    image : 'sushi.png',
+    description: 'Sushi rolling kit (includes bamboo mat), albacore belly tuna, rice, nori, hot sauce, black sesame seeds, scallions',
+    category: 'Japanese',
+    price: 35.00,
+    cost: 9.50
+};
+
+const mealKits = [
+    enchiladas, 
+    spaghetti,
+    burger,
+    ramen,
+    sushi
+];
+
+export defualt mealKits;
