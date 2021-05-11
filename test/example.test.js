deab48cfea141ba9228d44f3ad3ed0a3ab742b1f
BenFrancka <benfrancka@gmail.com>
2021-05-11 14:10:00 -0700

    tested findById function

--- test/example.test.js
+++ test/example.test.js
@@ -19,3 +19,61 @@ test('time to test a function', (expect) => {
 });
 
 
+// Data to test findById function
+const sushi = {
+    id: 'sushi',
+    name: 'Spicy Tuna Rolls',
+    image : 'sushi.png',
+    description: 'Sushi rolling kit (includes bamboo mat), albacore belly tuna, rice, nori, hot sauce, black sesame seeds, scallions',
+    category: 'Japanese',
+    price: 35.00,
+    cost: 9.50
+};
+
+const pizza = {
+    id: 'pizza',
+    name: 'Pepperoni Pizza',
+    image : 'pizza.png',
+    description: 'Detroit style deep dish pizza, includes dough kit, sauce, buffalo mozzarella, and aged pepperoni',
+    category: 'American/Italian',
+    price: 25.00,
+    cost: 6.00
+};
+
+const tacos = {
+    id: 'tacos',
+    name: 'Veggie Tacos',
+    image : 'tacos.png',
+    description: 'Corn tortillas, fire-roasted salsa, zuchinni, nopalitos, mushrooms, lime, and fresh cilantro',
+    category: 'Mexican',
+    price: 18.50,
+    cost: 5.75
+};
+
+
+const mealKits = [
+    sushi,
+    pizza,
+    tacos
+];
+
+//findById function test for expected
+test('takes in mealKits array and tacos id, returns tacos item', (expect) => {
+
+    const expected = tacos;
+
+    const actual = findById(mealKits, 'tacos');
+
+    expect.deepEqual(actual, expected);
+});
+
+//finbyId test for null
+test('takes in mealKits array and tacos id, returns tacos item', (expect) => {
+
+    const expected = null;
+
+    const actual = findById(mealKits, 'burritos');
+
+    expect.deepEqual(actual, expected);
+});
+
